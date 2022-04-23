47ab053e722be4b3a76f264af605a59c51274db8
Jack Grigg <thestr4d@gmail.com>
2022-04-23 11:01:58 +0200

    Add test vector from the vrf-r255 draft
    The test vector was generated by this implementation, so this is just
    a consistency check.

--- src/lib.rs
+++ src/lib.rs
@@ -281,3 +281,56 @@ impl Proof {
         beta_string.finalize().into()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_vector() {
+        let tv_sk = "3431c2b03533e280b23232e280b34e2c3132c2b03238e280b23131e280b34500";
+        let tv_pk = "54136cd90d99fbd1d4e855d9556efea87ba0337f2a6ce22028d0f5726fcb854e";
+        let tv_alpha = "633273702e6f72672f7672662d72323535";
+        let tv_H = "f245308737c2a888ba56448c8cdbce9d063b57b147e063ce36c580194ef31a63";
+        let tv_k_string = "b5eb28143d9defee6faa0c02ff0168b7ac80ea89fe9362845af15cabd100a91ed6251dfa52be36405576eca4a0970f91225b85c8813206d13bd8b42fd11a00fe";
+        let tv_k = "d32fcc5ae91ba05704da9df434f22fd4c2c373fdd8294bbb58bf27292aeec00a";
+        let tv_U = "9a30709d72de12d67f7af1cd8695ff16214d2d4600ae5f478873d2e7ed0ece73";
+        let tv_V = "5e727d972b11f6490b0b1ba8147775bceb1a2cb523b381fa22d5a5c0e97d4744";
+        let tv_pi = "0a97d961262fb549b4175c5117860f42ae44a123f93c476c439eddd1c0cff9265c805525233e2284dbed45e593b8eea31d5ca9734d72bcbba9738d5237f955f3b2422351149d1312503b6441a47c940c";
+        let tv_beta = "dd653f0879b48c3ef69e13551239bec4cbcc1c18fe8894de2e9e1c790e18273603bf1c6c25d7a797aeff3c43fd32b974d3fcbd4bcce916007097922a3ea3a794";
+
+        let tv_sk = hex::decode(tv_sk).unwrap();
+        let tv_pk = hex::decode(tv_pk).unwrap();
+        let tv_alpha = hex::decode(tv_alpha).unwrap();
+        let tv_H = hex::decode(tv_H).unwrap();
+        let tv_k_string = hex::decode(tv_k_string).unwrap();
+        let tv_k = hex::decode(tv_k).unwrap();
+        let tv_U = hex::decode(tv_U).unwrap();
+        let tv_V = hex::decode(tv_V).unwrap();
+        let tv_pi = hex::decode(tv_pi).unwrap();
+        let tv_beta = hex::decode(tv_beta).unwrap();
+
+        let sk = PrivateKey::from_bytes(tv_sk.try_into().unwrap()).unwrap();
+        let pk = PublicKey::from_bytes(tv_pk.try_into().unwrap()).unwrap();
+        assert_eq!(PublicKey::from(sk), pk);
+
+        let H = CompressedRistretto::from_slice(&tv_H).decompress().unwrap();
+        assert_eq!(encode_to_curve(pk.Y_bytes.as_bytes(), &tv_alpha), H);
+
+        let k = Scalar::from_canonical_bytes(tv_k.try_into().unwrap()).unwrap();
+        assert_eq!(
+            Scalar::from_bytes_mod_order_wide(&tv_k_string.try_into().unwrap()),
+            k
+        );
+        assert_eq!(sk.generate_nonce(H.compress().as_bytes()), k);
+
+        let U = CompressedRistretto::from_slice(&tv_U).decompress().unwrap();
+        let V = CompressedRistretto::from_slice(&tv_V).decompress().unwrap();
+        assert_eq!(k * B, U);
+        assert_eq!(k * H, V);
+
+        let pi = Proof::from_bytes(tv_pi.try_into().unwrap()).unwrap();
+        assert_eq!(sk.prove(&tv_alpha), pi);
+        assert_eq!(pk.verify(&tv_alpha, &pi), Some(tv_beta.try_into().unwrap()));
+    }
+}
